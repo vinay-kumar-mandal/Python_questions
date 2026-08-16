a522a920249a4501eb3b5f4d010ebfb93c82c004
VINAY-MANDAL <vinaykumarmandal847227@gmail.com>
2026-08-16 21:33:39 +0530

    Q 16, 17

---
+++ P-1_Q-16.py
@@ -0,0 +1,6 @@
+# Print all numbers from 1 to n.
+n = 5
+
+while n > 0:
+    n += 1
+    print(n)
---
+++ P-1_Q-17.py
@@ -0,0 +1,5 @@
+# Print all even numbers from 1 to n.
+n = 10
+while n > 0:
+    n += 2
+    print(n)
