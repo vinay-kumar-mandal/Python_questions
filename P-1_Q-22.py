1bacecef0a342b1b2b3d8c5a58a010442407fb28
VINAY-MANDAL <vinaykumarmandal847227@gmail.com>
2026-08-20 22:39:51 +0530

    Q 22,23 done

--- P-1_Q-22.py
+++ P-1_Q-22.py
@@ -1,1 +1,12 @@
 # Find the factorial of a number.
+num = 5
+factorial = 1
+
+if num < 0:
+    print("Factorial does not exist for negative numbers.")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"The factorial of {num} is {factorial}")
---
+++ P-1_Q-23.py
@@ -0,0 +1,7 @@
+# Count the number of digits in an integer.
+
+a = "Python"
+count = 0
+for char in a:
+    count += 1
+print(count)
